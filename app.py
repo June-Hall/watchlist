03b9150efa99eaae02c6de1ad79b75fc15a14c63
duke <chenjunhao100@gmail.com>
2023-03-02 15:49:24 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 import sys
-from flask import Flask, url_for, render_template
-from markupsafe import escape
-from flask_sqlalchemy import SQLAlchemy
 import click
+from flask import Flask, url_for, render_template
+# from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+# from gevent import pywsgi
+
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -15,60 +17,8 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
-
-@app.route('/home')
-@app.route('/index')
-def hello():
-    return '欢迎来到{{ user.name }} Watchlist!!!' + '<h1> HELLO TOTORO!</h1><img src="http://helloflask.com/totoro.gif">'
-
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User:{escape(name)}'
-
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='duke'))  # 输出：/user/greyli
-    print(url_for('user_page', name='bonnie'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return 'Test page'
-
-name = 'Duke'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
-
-@app.route('/')
-def index():
-    user = User.query.first()
-    movies = Movie.query.first()
-    return render_template('index.html', user=user, movies=movies)
-
-
-class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    name = db.Column(db.String(20))  # 名字
-
-
-class Movie(db.Model):  # 表名将会是 movie
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    title = db.Column(db.String(60))  # 电影标题
-    year = db.Column(db.String(4))  # 电影年份
-
 
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
 @click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
@@ -78,7 +28,6 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
-
 
 @app.cli.command()
 def forge():
@@ -108,3 +57,77 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+@app.route('/')
+def index():
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', movies=movies)
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# @app.route('/home')
+# @app.route('/index')
+# def hello():
+#     return '欢迎来到我的 Watchlist!!!' + '<h1> HELLO TOTORO!</h1><img src="http://helloflask.com/totoro.gif">'
+
+# @app.route('/user/<name>')
+# def user_page(name):
+#     return f'User:{escape(name)}'
+
+# @app.route('/test')
+# def test_url_for():
+#     # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+#     print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+#     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+#     print(url_for('user_page', name='duke'))  # 输出：/user/greyli
+#     print(url_for('user_page', name='bonnie'))  # 输出：/user/peter
+#     print(url_for('test_url_for'))  # 输出：/test
+#     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+#     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+#     return 'Test page'
+
+# server = pywsgi.WSGIServer(('0.0.0.0', 12345), app)
+# server.serve_forever()
+
+
+# name = 'Duke'
+# movies = [
+#     {'title': 'My Neighbor Totoro', 'year': '1988'},
+#     {'title': 'Dead Poets Society', 'year': '1989'},
+#     {'title': 'A Perfect World', 'year': '1993'},
+#     {'title': 'Leon', 'year': '1994'},
+#     {'title': 'Mahjong', 'year': '1996'},
+#     {'title': 'Swallowtail Butterfly', 'year': '1996'},
+#     {'title': 'King of Comedy', 'year': '1999'},
+#     {'title': 'Devils on the Doorstep', 'year': '1999'},
+#     {'title': 'WALL-E', 'year': '2008'},
+#     {'title': 'The Pork of Music', 'year': '2012'},
+# ]
+
+
+
+
+
+
+
+
+
+
