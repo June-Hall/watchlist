e0c3fcc559d44311943bdd939ec06f9a14490c6c
duke <chenjunhao100@gmail.com>
2023-03-12 13:10:35 +0800

    Translate the ui into Chinese.

--- watchlist/views.py
+++ watchlist/views.py
@@ -16,13 +16,13 @@ def index():
         year = request.form['year']
 
         if not title or not year or len(year) > 4 or len(title) > 60:
-            flash('Invalid input.')
+            flash('无效输入！')
             return redirect(url_for('index'))
 
         movie = Movie(title=title, year=year)
         db.session.add(movie)
         db.session.commit()
-        flash('Item created.')
+        flash('已创建！')
         return redirect(url_for('index'))
 
     movies = Movie.query.all()
@@ -39,13 +39,13 @@ def edit(movie_id):
         year = request.form['year']
 
         if not title or not year or len(year) > 4 or len(title) > 60:
-            flash('Invalid input.')
+            flash('无效输入！')
             return redirect(url_for('edit', movie_id=movie_id))
 
         movie.title = title
         movie.year = year
         db.session.commit()
-        flash('Item updated.')
+        flash('已更新！')
         return redirect(url_for('index'))
 
     return render_template('edit.html', movie=movie)
@@ -57,7 +57,7 @@ def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
     db.session.commit()
-    flash('Item deleted.')
+    flash('已删除！')
     return redirect(url_for('index'))
 
 
@@ -68,13 +68,13 @@ def settings():
         name = request.form['name']
 
         if not name or len(name) > 20:
-            flash('Invalid input.')
+            flash('无效输入！')
             return redirect(url_for('settings'))
 
         user = User.query.first()
         user.name = name
         db.session.commit()
-        flash('Settings updated.')
+        flash('已更新！')
         return redirect(url_for('index'))
 
     return render_template('settings.html')
@@ -87,17 +87,17 @@ def login():
         password = request.form['password']
 
         if not username or not password:
-            flash('Invalid input.')
+            flash('无效输入！')
             return redirect(url_for('login'))
 
         user = User.query.first()
 
         if username == user.username and user.validate_password(password):
             login_user(user)
-            flash('Login success.')
+            flash('登录成功！')
             return redirect(url_for('index'))
 
-        flash('Invalid username or password.')
+        flash('账号或密码无效！')
         return redirect(url_for('login'))
 
     return render_template('login.html')
@@ -107,5 +107,5 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash('Goodbye.')
+    flash('欢迎下次光临！')
     return redirect(url_for('index'))
